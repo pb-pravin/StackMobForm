c2beccbffbe51dc04b3dfe7b688b7a4147cda7b3
Wess Cope <wcope@me.com>
2013-01-25 13:10:29 -0500

    Initial Commit

---
+++ StackMobForm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StackMobForm
+//
+//  Created by Wess Cope on 1/25/13.
+//  Copyright (c) 2013 WessCope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMFAppDelegate class]));
+    }
+}
